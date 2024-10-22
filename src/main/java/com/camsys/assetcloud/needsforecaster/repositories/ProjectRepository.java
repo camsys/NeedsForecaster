5ece2760eaac7f7032f5818fd2c7f729e147eeb4
rbrown <rbrown@camsys.com>
2024-10-22 16:09:46 -0400

    NF-64 changed database query to treat fiscal year as integer instead of string

--- src/main/java/com/camsys/assetcloud/needsforecaster/repositories/ProjectRepository.java
+++ src/main/java/com/camsys/assetcloud/needsforecaster/repositories/ProjectRepository.java
@@ -14,7 +14,7 @@ public interface ProjectRepository extends CrudRepository<Project, Long> {
     //when comparing the string parameter to null, need to declare type for the null check (a postgres requirement)
     @Query("select p from Project p " +
             "where ((cast(:#{#filter.ownerOrganization} as string) is null) or p.ownerOrganization = :#{#filter.ownerOrganization}) " +
-            "and ((cast(:#{#filter.fiscalYear} as string) is null) or p.fiscalYear = :#{#filter.fiscalYear}) " +
+            "and (:#{#filter.fiscalYear} is null or p.fiscalYear = :#{#filter.fiscalYear}) " +
             "and ((cast(:#{#filter.projectType} as string) is null) or p.projectType = :#{#filter.projectType}) " +
             "and (:#{#filter.sogr} is null or p.sogr = :#{#filter.sogr}) " +
             "and (:#{#filter.manual} is null or p.manual = :#{#filter.manual}) "
