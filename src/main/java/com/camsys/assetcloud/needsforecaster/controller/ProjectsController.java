41edf0e81426a6a539def9c23a8e4797f058a8e7
rbrown <rbrown@camsys.com>
2024-10-22 12:10:06 -0400

    NF-61 added edit, new, and delete API calls for manual projects

--- src/main/java/com/camsys/assetcloud/needsforecaster/controller/ProjectsController.java
+++ src/main/java/com/camsys/assetcloud/needsforecaster/controller/ProjectsController.java
@@ -1,18 +1,14 @@
 package com.camsys.assetcloud.needsforecaster.controller;
 
+import com.camsys.assetcloud.needsforecaster.controller.exceptions.EntityNotFoundException;
 import com.camsys.assetcloud.needsforecaster.model.Project;
 import com.camsys.assetcloud.needsforecaster.model.ProjectFilter;
 import com.camsys.assetcloud.needsforecaster.repositories.ProjectRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class ProjectsController {
@@ -37,12 +33,78 @@ public class ProjectsController {
         return fiscalYears;//temporary list for UI use
     }
 
-    @PostMapping(value = "/api/projects", produces = "application/json")
+    @PostMapping(value = "/api/projects", consumes = "application/json", produces = "application/json")
     public List<Project> getProjects(@RequestBody(required = false) ProjectFilter filter) {
         if (filter == null)//no filter provided so return full list
             return projectRepository.list();
         else {
             return projectRepository.findByFilter(filter);
         }
+    }
+
+    @PostMapping(value = "/api/projects/new", consumes = "application/json", produces = "application/json")
+    public Project addProject(@RequestBody Project project) {
+        if (!project.manual) {
+            throw new IllegalArgumentException("Project must be manual to add with this API call");
+        }
+
+        if (project.isValid()) {
+            return projectRepository.save(project);
+        }
+        else {
+            throw new IllegalArgumentException("Invalid project: "+ project);
+        }
+    }
+
+    @DeleteMapping(value ="/api/projects/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteProject(@PathVariable(name="id") Long projectId) {
+        projectRepository.findById(projectId).map(toBeDeletedProject -> {
+            //can only delete manual projects
+            if (!toBeDeletedProject.manual) {
+                throw new IllegalArgumentException("Project must be manual to edit with this API call");
+            }
+            return toBeDeletedProject;
+        }).orElseThrow(() -> new EntityNotFoundException("Project", projectId));
+
+        //if made it this far, then delete the project
+        projectRepository.deleteById(projectId);
+    }
+
+    @PutMapping(value = "/api/projects/{id}", consumes = "application/json", produces = "application/json")
+    public Project editProject(@PathVariable(value="id") Long projectId, @RequestBody Project project) {
+
+        return projectRepository.findById(projectId).map(toBeEditedProject -> {
+                    //can only edit manual projects
+                    if (!toBeEditedProject.manual) {
+                        throw new IllegalArgumentException("Project must be manual to edit with this API call");
+                    }
+
+                    //check what data has changed
+                    boolean hasChanged = false;
+                    if (project.name != null && !toBeEditedProject.name.equals(project.name)) {
+                        toBeEditedProject.name = project.name;
+                        hasChanged = true;
+                    }
+                    if (project.description != null && !toBeEditedProject.description.equals(project.description)) {
+                        toBeEditedProject.description = project.description;
+                        hasChanged = true;
+                    }
+                    if (project.fiscalYear != null && !toBeEditedProject.fiscalYear.equals(project.fiscalYear)) {
+                        toBeEditedProject.fiscalYear = project.fiscalYear;
+                        hasChanged = true;
+                    }
+                    if (project.projectType != null && !toBeEditedProject.projectType.equals(project.projectType)) {
+                        toBeEditedProject.projectType = project.projectType;
+                        hasChanged = true;
+                    }
+
+
+                    if (hasChanged) {
+                        return projectRepository.save(toBeEditedProject);
+                    }
+                    return toBeEditedProject;
+                })
+                .orElseThrow(() -> new EntityNotFoundException("Project", projectId));
     }
 }
--- src/main/java/com/camsys/assetcloud/needsforecaster/model/Project.java
+++ src/main/java/com/camsys/assetcloud/needsforecaster/model/Project.java
@@ -31,4 +31,18 @@ public class Project {
     public boolean sogr;
 
     public boolean manual;
+
+    public boolean isValid() {
+        if (name == null || name.isEmpty()) {return false;}
+        if (description == null || description.isEmpty()) {return false;}
+        if (ownerOrganization == null || ownerOrganization.isEmpty()) {return false;}
+        if (projectType == null || projectType.isEmpty()) {return false;}
+        if (fiscalYear == null || fiscalYear < 2000) {return false;}
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return ownerOrganization + ": " + name;
+    }
 }
