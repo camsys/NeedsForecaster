1bc6e7f15b713cbc93c87fa9e5a3b678e837eae4
aandrade <aandrade@camsys.com>
2024-11-01 16:09:32 -0400

    [NF-28] Move and restyle search bar and add search functionality to projects page; remove "Manual" column from table

--- src/main/js/src/lib/IconInput.js
+++ src/main/js/src/lib/IconInput.js
@@ -6,8 +6,10 @@ export const IconInput = ({icon, name, type, label, handleChange, value, placeho
         <div className={"icon-input-group"}>
             {!!label && <label>{label}</label>}
             <div className={"icon-input"}>
-                {!!icon && <FontAwesomeIcon icon={icon}/>}
-                <input name={name} type={type} onChange={handleChange && (e => handleChange(e))} value={value} placeholder={placeholder}/>
+                <button className={"input-button"}>
+                    {!!icon && <FontAwesomeIcon icon={icon}/>}
+                    <input name={name} type={type} onChange={handleChange && (e => handleChange(e))} value={value} placeholder={placeholder}/>
+                </button>
             </div>
         </div>
     );
--- src/main/js/src/projects/Projects.js
+++ src/main/js/src/projects/Projects.js
@@ -10,10 +10,9 @@ import './Projects.css'
 import {IconInput} from "../lib/IconInput";
 
 export const Projects = () => {
-    // TODO: Add/edit projects in separate modal window
+    // TODO: Add/edit projects in separate page
     // TODO: Fiscal year formatted as YYYY when fiscal year is included in the label context, add FY to beginning when not
     // TODO: Move search bar to filters section and have it search on project name and description
-    // TODO: Add filter for manual projects
 
     let [organizations, setOrganizations] = useState([]);
     let [fiscalYears, setFiscalYears] = useState([]);
@@ -21,6 +20,7 @@ export const Projects = () => {
     let [filters, setFilters] = useState({});
     let [searchQuery, setSearchQuery] = useState('');
     let [projects, setProjects] = useState([]);
+    let [queriedProjects, setQueriedProjects] = useState([]);
     let [visibleProjects, setVisibleProjects] = useState([]);
     let [selectedProjects, setSelectedProjects] = useState([]);
     let [columns, setColumns] = useState({
@@ -29,8 +29,7 @@ export const Projects = () => {
         "name": true,
         "description": true,
         "sogr": true,
-        "projectType": true,
-        "manual": true
+        "projectType": true
     });
     let [page, setPage] = useState(1);
     let [pageSize, setPageSize] = useState(10);
@@ -52,8 +51,7 @@ export const Projects = () => {
         "name": "Title",
         "description": "Project Description",
         "sogr": "SOGR",
-        "projectType": "Type",
-        "manual": "Manual"
+        "projectType": "Type"
     }
 
     const updateFilters = (filter, value) => {
@@ -84,6 +82,7 @@ export const Projects = () => {
                     .json()
                     .then((data) => {
                         setProjects(data);
+                        setQueriedProjects(data.filter(p => !!searchQuery ? (p.name.includes(searchQuery) || p.description.includes(searchQuery)) : p));
                         setLoading(false);
                     })
             })
@@ -96,7 +95,6 @@ export const Projects = () => {
     const formatTableData = (column, data) => {
         switch (column) {
             case 'sogr':
-            case 'manual':
                 return data && <FontAwesomeIcon icon={'circle-check'} />;
             default:
                 return data;
@@ -110,12 +108,12 @@ export const Projects = () => {
     const changePage = (pageNum) => {
         // TODO: Pagination will be frontend
         setPage(pageNum);
-        setVisibleProjects(projects.slice(pageSize * (pageNum - 1), pageSize * pageNum));
+        setVisibleProjects(queriedProjects.slice(pageSize * (pageNum - 1), pageSize * pageNum));
         refreshSelectablePages();
     }
 
     const refreshSelectablePages = () => {
-        let numPages = Math.floor((projects.length - 1) / pageSize) + 1;
+        let numPages = Math.floor((queriedProjects.length - 1) / pageSize) + 1;
         if (numPages <= 0) {
             setSelectablePages([]);
         }
@@ -149,6 +147,15 @@ export const Projects = () => {
 
     const addProject = () => {
         console.log("Not really adding new project.");
+    }
+
+    const executeSearch = (query) => {
+        setSearchQuery(query);
+        if(query.length > 2) {
+            setQueriedProjects(projects.filter(p => (p.name.includes(query) || p.description.includes(query))));
+        } else {
+            setQueriedProjects(projects);
+        }
     }
 
     useEffect(() => {
@@ -200,9 +207,9 @@ export const Projects = () => {
     }, [filters]);
 
     useEffect(() => {
-        setVisibleProjects(projects.slice(pageSize * (page - 1), pageSize * page))
+        setVisibleProjects(queriedProjects.slice(pageSize * (page - 1), pageSize * page))
         refreshSelectablePages();
-    }, [projects, page, pageSize]);
+    }, [queriedProjects, page, pageSize]);
 
     useEffect(() => {
         setPage(1);
@@ -219,14 +226,14 @@ export const Projects = () => {
                 <h2>Filters</h2>
                 <div className={"filters-container"}>
                     <DropdownInput name={"organization"} label={"Organization"} options={organizations.map(o => ({key: o.orgKey, value: o.orgKey, name: o.name}))} includeBlank={"Select"} handleChange={(e)=>updateFilters("ownerOrganization", e.target.value)}/>
-                    <DropdownInput name={"fy"} label={"FY"} options={fiscalYears.map(o => ({key: `fy_${o.toString()}`, value: o, name: o.toString()}))} includeBlank={"Select"} handleChange={(e)=>updateFilters("fiscalYear", parseInt(e.target.value))}/>
+                    <DropdownInput name={"fy"} label={"FY"} options={fiscalYears.map(fy => ({key: `fy_${fy.toString()}`, value: fy, name: fy.toString()}))} includeBlank={"Select"} handleChange={(e)=>updateFilters("fiscalYear", parseInt(e.target.value))}/>
                     <DropdownInput name={"sogr"} label={"SOGR"} options={[{key: "sogr_true", value: true, name: "Yes"},{key: "sogr_false", value: false, name: "No"}]} includeBlank={"Select"} handleChange={(e)=>updateFilters("sogr", e.target.value)}/>
-                    <DropdownInput name={"project_type"} label={"Type"} options={projectTypes.map(t => ({key: t.key, value: t.key, name: t.name}))} includeBlank={"Select"} handleChange={(e)=>updateFilters("projectType", e.target.value)}/>
+                    <DropdownInput name={"project_type"} label={"Type"} options={projectTypes.map(t => ({key: t.key, value: t.name, name: t.name}))} includeBlank={"Select"} handleChange={(e)=>updateFilters("projectType", e.target.value)}/>
+                    <IconInput icon={'magnifying-glass'} name={"search_bar"} type={"text"} placeholder={"Search Table..."} value={searchQuery} handleChange={(e) => executeSearch(e.target.value)}/>
                 </div>
             </div>
             <div className={"projects-table-container"}>
                 <div className={"table-actions"}>
-                    <IconInput icon={'magnifying-glass'} name={"searchBar"} type={"text"} placeholder={"Search Table..."} value={searchQuery} handleChange={(e) => setSearchQuery(e.target.value)}/>
                     <ActionsButton actions={exportActionsMenuItems} icon={"file-arrow-down"} label={"Export"}/>
                     <ActionsButton actions={Object.keys(columnNameLabels).map(c => ({
                         text: columnNameLabels[c],
@@ -247,7 +254,7 @@ export const Projects = () => {
                             {visibleProjects.map(p => <>
                                 <tr>
                                     <td className={"icon-column"} onClick={()=>selectProject(p)}><FontAwesomeIcon icon={selectedProjects.includes(p) ? "fa-regular fa-square-check" : "fa-regular fa-square"}/></td>
-                                    {Object.keys(columnNameLabels).filter(c => columns[c]).map(col => <td className={['sogr','manual'].includes(col) ? "icon-column" : ""}>{formatTableData(col, p[col])}</td>)}
+                                    {Object.keys(columnNameLabels).filter(c => columns[c]).map(col => <td className={col === "sogr" ? "icon-column" : ""}>{formatTableData(col, p[col])}</td>)}
                                 </tr>
                             </>)}
                         </tbody>
@@ -257,17 +264,17 @@ export const Projects = () => {
                     <div className={"page-size-container"}>
                         <DropdownInput name={"page_size"} options={[{key: "page_size_10", value: 10, name: "10"},{key: "page_size_20", value: 20, name: "20"},{key: "page_size_50", value: 50, name: "50"},{key: "page_size_100", value: 100, name: "100"}]} handleChange={(e)=>setPageSize(e.target.value)} defaultValue={pageSize} noArrow={true}/>Rows per page
                     </div>
-                    <p className={"page-info"}>Showing <b>{pageSize * (page - 1) + 1} to {pageSize * page < projects.length ? pageSize * page : projects.length}</b> of {projects.length} rows</p>
+                    <p className={"page-info"}>Showing <b>{pageSize * (page - 1) + 1} to {pageSize * page < queriedProjects.length ? pageSize * page : queriedProjects.length}</b> of {queriedProjects.length} rows</p>
                     <div className={"page-selector"}>
                         {page > 1 && <FontAwesomeIcon icon={"fa-angle-left"} onClick={()=>setPage(page - 1)}/>}
                         {selectablePages.map((p) => (
                             <>
                                 {p === selectablePages[1] && page > 4 && <div className={"bottom-align"}>...</div>}
                                 <a className={p === page ? "current-page" : ""} href={void(0)} onClick={()=>setPage(p)}>{p}</a>
-                                {p === selectablePages[selectablePages.length-2] && p < Math.floor((projects.length - 1) / pageSize) && <div className={"bottom-align"}>...</div>}
+                                {p === selectablePages[selectablePages.length-2] && p < Math.floor((queriedProjects.length - 1) / pageSize) && <div className={"bottom-align"}>...</div>}
                             </>
                         ))}
-                        {page <= Math.floor((projects.length - 1) / pageSize) && <FontAwesomeIcon icon={"fa-angle-right"} onClick={()=>setPage(page + 1)}/>}
+                        {page <= Math.floor((queriedProjects.length - 1) / pageSize) && <FontAwesomeIcon icon={"fa-angle-right"} onClick={()=>setPage(page + 1)}/>}
                     </div>
                 </div>
             </div>
