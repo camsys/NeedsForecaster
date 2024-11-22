90d921dea52812a831abfc60bdbcf05b5e715af2
rbrown <rbrown@camsys.com>
2024-11-22 11:15:10 -0500

    NF-76 changed fiscal year to start July 1 instead of Jan 1

--- src/main/java/com/camsys/assetcloud/needsforecaster/services/Utility.java
+++ src/main/java/com/camsys/assetcloud/needsforecaster/services/Utility.java
@@ -3,12 +3,18 @@ package com.camsys.assetcloud.needsforecaster.services;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 @Service
 public class Utility {
     public static Integer getFiscalYear(LocalDate date) {
-        //TODO: implement actual logic to calculate the fiscal year the given date is associated with - for now just assume fiscal year is the calendar year
-        return date.getYear();
+        //using July 1 as start of a fiscal year
+        int dateYear = date.getYear();
+        Month dateMonth = date.getMonth();
+        if (Month.JULY.compareTo(dateMonth) > 0) {//JULY greater than date month
+            return dateYear;//date is before July 1 so use current calendar year
+        }
+        else return dateYear + 1;//use next calendar year as the fiscal year for this date
     }
 
     public static Integer getCurrentFiscalYear() {
